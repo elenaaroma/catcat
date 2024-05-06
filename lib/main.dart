83f88cedc26bd5808d4d476503425e4c633d891a
Elena <elenaromadam@gmail.com>
2024-05-06 20:31:19 +0200

    pantalla completa

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:catcat/catcat.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  //para que se vea en pantalla completa
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
+
   catcat game = catcat();
 
   runApp(GameWidget(game: game));
