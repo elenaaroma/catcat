dfbe623763156271e30e0bddd58c435ada7be463
Elena <elenaromadam@gmail.com>
2024-05-06 23:12:06 +0200

    commit

--- lib/catcat.dart
+++ lib/catcat.dart
@@ -1,3 +1,13 @@
+import 'dart:async';
+
+import 'package:catcat/levels/level.dart';
 import 'package:flame/game.dart';
 
-class catcat extends FlameGame {}
+class catcat extends FlameGame {
+  @override
+  FutureOr<void> onLoad() {
+    add(Level());
+
+    return super.onLoad();
+  }
+}
