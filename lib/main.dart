8102d3faf4c03f86405c4f538a0933758fc79b2d
Elena <elenaromadam@gmail.com>
2024-05-06 21:06:56 +0200

    mapa del nivel 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:catcat/catcat.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,5 +12,7 @@ void main() {
 
   catcat game = catcat();
 
-  runApp(GameWidget(game: game));
+  runApp(
+    GameWidget(game: kDebugMode ? catcat() : game),
+  );
 }
