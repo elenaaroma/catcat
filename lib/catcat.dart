17f398415bde0d08e3359267484193ea03fdac4a
Elena <elenaromadam@gmail.com>
2024-05-08 17:07:18 +0200

    commit semuestra mapa

--- lib/catcat.dart
+++ lib/catcat.dart
@@ -1,12 +1,24 @@
 import 'dart:async';
 
 import 'package:catcat/levels/level.dart';
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 
 class catcat extends FlameGame {
   @override
+  Color backgroundColor() => const Color(0xFF3f3851);
+
+  late final CameraComponent cam;
+  final world = Level();
+
+  @override
   FutureOr<void> onLoad() {
-    add(Level());
+    cam = CameraComponent.withFixedResolution(
+        world: world, width: 640, height: 380);
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, world]);
 
     return super.onLoad();
   }
