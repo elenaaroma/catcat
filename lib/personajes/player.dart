235be69a15d4ef4b47e8e24643cf4e306255f9fd
elenaromatechnaid <166527266+elenaromatechnaid@users.noreply.github.com>
2024-05-13 12:10:43 +0200

    commit mas animaciones para el personaje

--- lib/personajes/player.dart
+++ lib/personajes/player.dart
@@ -3,10 +3,12 @@ import 'dart:async';
 import 'package:catcat/catcat.dart';
 import 'package:flame/components.dart';
 
-enum PlayerState { idle, run, jump, dead }
+enum PlayerState { idle, run, dead }
 
 class Player extends SpriteAnimationGroupComponent with HasGameRef<Catcat> {
   late final SpriteAnimation idleAnimation;
+  late final SpriteAnimation runAnimation;
+  late final SpriteAnimation deadAnimation;
 
   @override
   FutureOr<void> onLoad() {
@@ -16,10 +18,25 @@ class Player extends SpriteAnimationGroupComponent with HasGameRef<Catcat> {
   }
 
   void _loadAllAnmations() {
+// animacion idle para rojo
     idleAnimation = SpriteAnimation.fromFrameData(
       game.images.fromCache('characters/red-knight/idle.png'),
       SpriteAnimationData.sequenced(
           amount: 5, stepTime: 0.5, textureSize: Vector2.all(64)),
+    );
+
+// animacion run para rojo
+    runAnimation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('characters/red-knight/run.png'),
+      SpriteAnimationData.sequenced(
+          amount: 8, stepTime: 0.5, textureSize: Vector2.all(64)),
+    );
+
+// animacion dead para rojo
+    deadAnimation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('characters/red-knight/dead.png'),
+      SpriteAnimationData.sequenced(
+          amount: 7, stepTime: 0.5, textureSize: Vector2.all(64)),
     );
 
     animations = {
