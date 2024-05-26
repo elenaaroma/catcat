05f04da63c97365e2bbab728247a4ec42ce3c9e4
elenaaroma <elenaromadam@gmail.com>
2024-05-26 23:41:20 +0200

    commit musica menu con boton

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,8 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:catcat/screens/register_dialog.dart';
 import 'package:catcat/screens/login_dialog.dart';
 import 'package:catcat/screens/play_screen.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late AudioPlayer _audioPlayer;
+  bool _isMusicPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+  }
+
+  Future<void> _playMusic() async {
+    try {
+      await _audioPlayer.setReleaseMode(
+          ReleaseMode.loop); // Configurar el modo de reproducción en bucle
+      if (kIsWeb) {
+        await _audioPlayer.play(UrlSource('assets/audio/musica_menu.mp3'));
+      } else {
+        await _audioPlayer
+            .play(DeviceFileSource('assets/audio/musica_menu.mp3'));
+      }
+      setState(() {
+        _isMusicPlaying = true;
+      });
+      print('Música reproduciéndose');
+    } catch (e) {
+      print('Error al reproducir música: $e');
+    }
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +60,15 @@ class HomeScreen extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    if (!_isMusicPlaying) {
+                      _playMusic();
+                    }
+                  },
+                  child: Text('Iniciar Música'),
+                ),
+                SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {
                     showDialog(
