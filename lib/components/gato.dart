e345a8a0a876b0befb2d9638b22397ce18330a02
elenaromatechnaid <elenaromadam@gmail.com>
2024-05-28 14:40:16 +0200

    commit inicio colision gato que deberia cambiar imagen

--- lib/components/gato.dart
+++ lib/components/gato.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 
 import 'package:catcat/catcat.dart';
+import 'package:catcat/components/player.dart';
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 
-class Gato extends SpriteAnimationComponent with HasGameRef<Catcat> {
+class Gato extends SpriteAnimationComponent
+    with HasGameRef<Catcat>, CollisionCallbacks {
   Gato({
     super.position,
     super.size,
@@ -11,11 +14,33 @@ class Gato extends SpriteAnimationComponent with HasGameRef<Catcat> {
 
   @override
   FutureOr<void> onLoad() {
+    debugMode = true;
+
+    add(RectangleHitbox(
+        position: Vector2(5, 5),
+        size: Vector2(20, 20),
+        collisionType: CollisionType.passive));
+
     animation = SpriteAnimation.fromFrameData(
         game.images.fromCache('cat/idle.png'),
         SpriteAnimationData.sequenced(
             amount: 4, stepTime: 0.1, textureSize: Vector2.all(32)));
 
     return super.onLoad();
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    if (other is Player) {
+      _reachedCheckpoint();
+    }
+    super.onCollision(intersectionPoints, other);
+  }
+
+  void _reachedCheckpoint() {
+    animation = SpriteAnimation.fromFrameData(
+        game.images.fromCache('cat/fin.png'),
+        SpriteAnimationData.sequenced(
+            amount: 6, stepTime: 0.1, textureSize: Vector2.all(32)));
   }
 }
